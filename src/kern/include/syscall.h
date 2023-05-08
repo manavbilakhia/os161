a929a4b7d205dce0c989f429b15fb745d51ae7ce
John Collins <collinsj@union.edu>
2023-05-08 19:06:38 -0400

    Adding the _exit syscall basic implementation

--- src/kern/include/syscall.h
+++ src/kern/include/syscall.h
@@ -59,4 +59,12 @@ __DEAD void enter_new_process(int argc, userptr_t argv, userptr_t env,
 int sys_reboot(int code);
 int sys___time(userptr_t user_seconds, userptr_t user_nanoseconds);
 
+
+/*
+ * Causes the current process to exit.
+ */
+
+void _exit(int exitcode);
+
+
 #endif /* _SYSCALL_H_ */
---
+++ src/kern/syscall/_exit.c
@@ -0,0 +1,18 @@
+#include <types.h>
+#include <kern/errno.h>
+#include <lib.h>
+#include <uio.h>
+#include <proc.h>
+#include <current.h>
+#include <addrspace.h>
+#include <vnode.h>
+#include <elf.h>
+#include <syscall.h>
+#include <thread.h>
+
+void
+_exit(int exitcode){
+    KASSERT(curthread != NULL);
+    (void) exitcode;
+    thread_exit();
+}
