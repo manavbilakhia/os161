81061dd798a970f984899ee4169cb8d6d7166b53
Manav Bilakhia <bilakhim@union.edu>
2023-05-18 15:51:08 -0400

    open and werite need error checks, read is done do not change

--- src/kern/syscall/write.c
+++ src/kern/syscall/write.c
@@ -10,6 +10,7 @@
 #include <kern/fcntl.h>
 #include <copyinout.h>
 #include <file_table.h>
+#include <kern/unistd.h>
 
 //ssize_t
 //sys_write(int fd, const void *buf, size_t nbytes){
@@ -25,23 +26,22 @@
 
 ssize_t
 sys_write(int fd, const void *buf, size_t nbytes){
-//code to write on the terminal
-    if (fd < 3)
-    {
-        char *print;    
-        print = (char *) kmalloc(nbytes); 
-        int ret = copyin((const_userptr_t) buf, print, nbytes);
-        (void) ret;
-        kprintf("%s", ((char *) buf));
-        return nbytes;   
-    }
-    else
-    {
     // Check if fd is valid
-    if (fd < 0 || fd >= MAX_FILES) {
+    if (fd < 0 || fd >= MAX_FILES || buf == NULL) {
         return -EBADF;
     }
 
+    // handle console output
+    if (fd == STDOUT_FILENO || fd == STDERR_FILENO) {
+        size_t i;
+        const char *buffer = (const char *)buf;
+
+        for (i = 0; i < nbytes; i++) {
+            putch(buffer[i]);
+        }
+
+        return i; // return the number of characters written
+    }
     // Look up the file and check wiuth some basic error codes
     struct file *file = curproc->p_filetable->files[fd];
     if (file == NULL || !(file->flags & O_WRONLY || file->flags & O_RDWR || file->flags & O_ACCMODE || file->flags & O_RDONLY)) {
@@ -68,5 +68,4 @@ sys_write(int fd, const void *buf, size_t nbytes){
     // Return the number of bytes written
     return nbytes - write_uio.uio_resid;
     }
-}
 
